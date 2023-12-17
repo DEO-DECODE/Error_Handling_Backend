4a325ef1ed551b5ed123f794f5c1022c5bc27975
Deo Raj <d.jrajsingh81@gmail.com>
2023-12-18 00:13:37 +0530

    Ab ye A and B print krega

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 import express from "express";
 const app = express();
-app.get("/", (req, res) => {
+app.get(
+  "/",
+  (req, res, next) => {
+    console.log("A");
+  },
+  (req, res, next) => {
+    console.log("B");
+  },
+  (req, res, next) => {
+    console.log("C");
+  }
+);
+app.use((err, req, res) => {
   res.status(404).json({
-    message: "Not found",
+    message: err.message,
   });
 });
 app.listen(4000, () => {
